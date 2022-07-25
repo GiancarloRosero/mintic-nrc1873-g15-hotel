87b922614802e0fd32d21a37eb7b056083beae6f
Giancarlo Rosero <roserogiancarlo@gmail.com>
2022-07-25 00:20:19 -0500

    configurar tsconfig

--- src/app/components/shared/comment/comment.component.ts
+++ src/app/components/shared/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClickEvent, HoverRatingChangeEvent, RatingChangeEvent } from 'angular-star-rating';
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
+  onClickResult: ClickEvent;
+  onHoverRatingChangeResult: HoverRatingChangeEvent;
+  onRatingChangeResult: RatingChangeEvent;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onClick = ($event: ClickEvent) => {
+    console.log('onClick $event: ', $event);
+    this.onClickResult = $event;
+  };
+
+  onRatingChange = ($event: RatingChangeEvent) => {
+    console.log('onRatingUpdated $event: ', $event);
+    this.onRatingChangeResult = $event;
+  };
+
+  onHoverRatingChange = ($event: HoverRatingChangeEvent) => {
+    console.log('onHoverRatingChange $event: ', $event);
+    this.onHoverRatingChangeResult = $event;
+  };
 
 }
