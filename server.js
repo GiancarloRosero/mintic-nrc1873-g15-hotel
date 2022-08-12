5b527ee84d675be8decea92e88802846b7dd73ad
Giancarlo Rosero <pgiancarlo@uninorte.edu.co>
2022-08-12 10:55:09 -0500

    ajuste variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express();
 app.use(express.static(__dirname+'/dist/mintic-nrc1873-g15-hotel-front'));
 
 app.get('/*', (req, res) =>{
-    resp.sendFile(__dirname+'/dist/mintic-nrc1873-g15-hotel-front/index.html');
+    res.sendFile(__dirname+'/dist/mintic-nrc1873-g15-hotel-front/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
